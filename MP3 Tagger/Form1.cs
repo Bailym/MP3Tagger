2da82e7dd020727784f8c45c56acfbcbb293bb82
Bailym <bm39@hotmail.co.uk>
2021-12-20 17:10:33 +0000

    push

---
+++ MP3 Tagger/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MP3_Tagger
+{
+
+    public partial class MP3Tagger : Form
+    {
+        public void GetTags() {
+            //Read Tags
+            string Selected = Convert.ToString(lstFiles.SelectedItem);
+            TagLib.File file = TagLib.File.Create(Selected);
+            string title = file.Tag.Title;
+            string album = file.Tag.Album;
+            string artist = file.Tag.FirstPerformer;
+            string duration = file.Properties.Duration.ToString();
+            uint number = file.Tag.Track;
+            var art = file.Tag.Pictures.FirstOrDefault();
+
+            //Display Tags 
+            txtTitle.Text = title;
+            txtArtist.Text = artist;
+            txtAlbum.Text = album;
+            txtLength.Text = duration.Substring(0, 9);
+            txtNumber.Text = Convert.ToString(number);
+
+            //Album Art
+            if (art != null)
+            {
+                var ms = new MemoryStream();
+                byte[] PictureData = art.Data.Data;
+                ms.Write(PictureData, 0, Convert.ToInt32(PictureData.Length));
+                var bm = new Bitmap(ms, false);
+                ms.Dispose();
+                pctArt.Image = bm;
+            }
+            else
+            {
+                pctArt.Image = null;
+            }
+        }
+
+
+
+
+
+
+
+
+        public MP3Tagger()
+        {
+            InitializeComponent();
+
+
+
+
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+                string FolderPath = "";
+                FolderBrowserDialog SelectFile = new FolderBrowserDialog(); //Create Class
+                if (SelectFile.ShowDialog() == DialogResult.OK) //OK Dialog
+                {
+                    FolderPath = SelectFile.SelectedPath;
+                    txtPath.Text = FolderPath; //Shows Path In TextBox
+                    string[] MP3Files = Directory.GetFiles(@FolderPath, "*.MP3");
+                    lstFiles.Items.Clear();
+                    foreach (string file in MP3Files)
+                    {
+                        lstFiles.Items.Add(file);
+                    }
+                }
+
+
+                File.WriteAllText("FolderPath.txt", "");
+                File.WriteAllText("FolderPath.txt", FolderPath); 
+
+        
+
+        
+    
+
+           
+         
+            
+               
+            
+
+            if (lstFiles.Items.Count == 0) {
+                lstFiles.Items.Add("No MP3 Files Found");
+                txtTitle.Text = "";
+                txtAlbum.Text = "";
+                txtArtist.Text = "";
+                txtLength.Text = "";
+                txtNumber.Text = "";
+                pctArt.Image = null;
+                lstFiles.Enabled = false;
+            }
+            else
+            {
+                lstFiles.Enabled = true;
+            }
+
+            txtTitle.Enabled = true;
+            txtAlbum.Enabled = true;
+            txtArtist.Enabled = true;
+            txtNumber.Enabled = true;
+            txtLength.Enabled = true;
+            pctArt.Enabled = true;
+            btnSave.Enabled = true;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            txtTitle.Enabled = false;
+            txtAlbum.Enabled = false;
+            txtArtist.Enabled = false;
+            txtNumber.Enabled = false;
+            txtLength.Enabled = false;
+            pctArt.Enabled = false;
+            btnSave.Enabled = false;
+
+            if (File.Exists("FolderPath.txt"))
+            {
+
+                txtPath.Text = File.ReadAllText("FolderPath.txt");
+
+                if (txtPath.Text != "")
+                {
+                    string[] MP3Files = Directory.GetFiles(txtPath.Text, "*.MP3");
+                    foreach (string file in MP3Files)
+                    {
+                        lstFiles.Items.Add(file);
+                    }
+
+                    txtTitle.Enabled = true;
+                    txtAlbum.Enabled = true;
+                    txtArtist.Enabled = true;
+                    txtNumber.Enabled = true;
+                    txtLength.Enabled = true;
+                    pctArt.Enabled = true;
+                    btnSave.Enabled = true;
+                }
+                else
+                {
+                    File.Create("txtPath.txt");
+                }
+
+
+
+
+
+
+            }
+        }
+
+        private void ShowFiles_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void lstFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetTags();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (lstFiles.SelectedIndex >= 0)
+            {
+                string Selected = Convert.ToString(lstFiles.SelectedItem);
+                TagLib.File file = TagLib.File.Create(Selected);
+                file.Tag.Title = txtTitle.Text; //Update Tags With New Data.
+                file.Tag.Album = txtAlbum.Text;
+                file.Tag.Performers = new String[1] { txtArtist.Text };
+                file.Tag.Track = Convert.ToUInt16(txtNumber.Text);
+
+                try
+                {
+                    TagLib.Picture Art = new TagLib.Picture();
+                    MemoryStream ms = new MemoryStream();
+                    pctArt.Image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    ms.Position = 0;
+                    Art.Data = TagLib.ByteVector.FromStream(ms);
+                    file.Tag.Pictures = new TagLib.IPicture[1] { Art };
+                    file.Save();
+                    ms.Close();
+                }
+                catch
+                {
+                }
+                file.Save();
+
+
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please Select An MP3 File");
+            }
+        }
+
+        public void pctArt_Click(object sender, EventArgs e)
+        {
+            try { string Selected = Convert.ToString(lstFiles.SelectedItem);
+                TagLib.File file = TagLib.File.Create(Selected);
+                OpenFileDialog SelectCover = new OpenFileDialog();
+                SelectCover.Filter = "Image Files(*.JPG; *.JPEG; *.GIF; *.BMP *.PNG)|*.JPG; *.JPEG; *.GIF; *.BMP,*.PNG";
+                string FolderPath = SelectCover.FileName;
+                if (SelectCover.ShowDialog() == DialogResult.OK) //OK Dialog 
+                {
+                    pctArt.Image = new Bitmap(SelectCover.FileName);
+                }
+            }
+            catch { }
+
+        }
+    }
+    
+    }
+
+
+
+
---
+++ MP3 Tagger/Main.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace MP3_Tagger
+{
+    partial class MP3Tagger
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MP3Tagger));
+            this.btnSelectDirectory = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblArtist = new System.Windows.Forms.Label();
+            this.lblAlbum = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtArtist = new System.Windows.Forms.TextBox();
+            this.txtAlbum = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.lblAlbumArt = new System.Windows.Forms.Label();
+            this.pctArt = new System.Windows.Forms.PictureBox();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctArt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelectDirectory
+            // 
+            this.btnSelectDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectDirectory.Location = new System.Drawing.Point(530, 478);
+            this.btnSelectDirectory.Name = "btnSelectDirectory";
+            this.btnSelectDirectory.Size = new System.Drawing.Size(102, 57);
+            this.btnSelectDirectory.TabIndex = 0;
+            this.btnSelectDirectory.Text = "Select Directory";
+            this.btnSelectDirectory.UseVisualStyleBackColor = true;
+            this.btnSelectDirectory.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Enabled = false;
+            this.txtPath.Location = new System.Drawing.Point(138, 497);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(386, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.Location = new System.Drawing.Point(246, 26);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(385, 446);
+            this.lstFiles.TabIndex = 2;
+            this.lstFiles.SelectedIndexChanged += new System.EventHandler(this.lstFiles_SelectedIndexChanged);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(9, 26);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(27, 13);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.Text = "Title";
+            // 
+            // lblArtist
+            // 
+            this.lblArtist.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblArtist.AutoSize = true;
+            this.lblArtist.Location = new System.Drawing.Point(9, 65);
+            this.lblArtist.Name = "lblArtist";
+            this.lblArtist.Size = new System.Drawing.Size(30, 13);
+            this.lblArtist.TabIndex = 4;
+            this.lblArtist.Text = "Artist";
+            // 
+            // lblAlbum
+            // 
+            this.lblAlbum.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblAlbum.AutoSize = true;
+            this.lblAlbum.Location = new System.Drawing.Point(9, 104);
+            this.lblAlbum.Name = "lblAlbum";
+            this.lblAlbum.Size = new System.Drawing.Size(39, 13);
+            this.lblAlbum.TabIndex = 5;
+            this.lblAlbum.Text = "Album ";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTitle.Location = new System.Drawing.Point(12, 42);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(228, 20);
+            this.txtTitle.TabIndex = 6;
+            // 
+            // txtArtist
+            // 
+            this.txtArtist.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtArtist.Location = new System.Drawing.Point(12, 81);
+            this.txtArtist.Name = "txtArtist";
+            this.txtArtist.Size = new System.Drawing.Size(228, 20);
+            this.txtArtist.TabIndex = 7;
+            // 
+            // txtAlbum
+            // 
+            this.txtAlbum.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAlbum.Location = new System.Drawing.Point(12, 120);
+            this.txtAlbum.Name = "txtAlbum";
+            this.txtAlbum.Size = new System.Drawing.Size(228, 20);
+            this.txtAlbum.TabIndex = 8;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(12, 443);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(110, 89);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblLength
+            // 
+            this.lblLength.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblLength.AutoSize = true;
+            this.lblLength.Location = new System.Drawing.Point(9, 143);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(40, 13);
+            this.lblLength.TabIndex = 10;
+            this.lblLength.Text = "Length";
+            // 
+            // txtLength
+            // 
+            this.txtLength.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLength.Location = new System.Drawing.Point(12, 159);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.ReadOnly = true;
+            this.txtLength.Size = new System.Drawing.Size(110, 20);
+            this.txtLength.TabIndex = 11;
+            // 
+            // lblAlbumArt
+            // 
+            this.lblAlbumArt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblAlbumArt.AutoSize = true;
+            this.lblAlbumArt.Location = new System.Drawing.Point(9, 182);
+            this.lblAlbumArt.Name = "lblAlbumArt";
+            this.lblAlbumArt.Size = new System.Drawing.Size(52, 13);
+            this.lblAlbumArt.TabIndex = 12;
+            this.lblAlbumArt.Text = "Album Art";
+            // 
+            // pctArt
+            // 
+            this.pctArt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pctArt.BackColor = System.Drawing.SystemColors.Control;
+            this.pctArt.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pctArt.Image = ((System.Drawing.Image)(resources.GetObject("pctArt.Image")));
+            this.pctArt.InitialImage = ((System.Drawing.Image)(resources.GetObject("pctArt.InitialImage")));
+            this.pctArt.Location = new System.Drawing.Point(12, 198);
+            this.pctArt.MaximumSize = new System.Drawing.Size(200, 173);
+            this.pctArt.Name = "pctArt";
+            this.pctArt.Size = new System.Drawing.Size(200, 173);
+            this.pctArt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctArt.TabIndex = 13;
+            this.pctArt.TabStop = false;
+            this.pctArt.Click += new System.EventHandler(this.pctArt_Click);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(125, 143);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(14, 13);
+            this.lblNumber.TabIndex = 14;
+            this.lblNumber.Text = "#";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNumber.Location = new System.Drawing.Point(128, 159);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(112, 20);
+            this.txtNumber.TabIndex = 15;
+            // 
+            // MP3Tagger
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 544);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.pctArt);
+            this.Controls.Add(this.lblAlbumArt);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.lblLength);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtAlbum);
+            this.Controls.Add(this.txtArtist);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.lblAlbum);
+            this.Controls.Add(this.lblArtist);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnSelectDirectory);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(660, 583);
+            this.MinimumSize = new System.Drawing.Size(660, 583);
+            this.Name = "MP3Tagger";
+            this.Tag = "MP3 Tagger";
+            this.Text = "MP3 Tagger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctArt)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectDirectory;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblArtist;
+        private System.Windows.Forms.Label lblAlbum;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtArtist;
+        private System.Windows.Forms.TextBox txtAlbum;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Label lblAlbumArt;
+        private System.Windows.Forms.PictureBox pctArt;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.TextBox txtNumber;
+    }
+}
+
